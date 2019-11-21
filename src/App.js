14c43dbe41f7e8ab5ba1da23888b50074ad1b8ba
feresCr7 <feressayari5@gmail.com>
2019-11-21 14:23:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+class Credit extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: '****************',
+      ValidThru: '11/50',
+      Valid: '',
+      name: 'CARDHOLDER',
+    }
+  }
+  numberChanging = event => {
+    this.setState({
+      number: event.target.value,
+    });
+  }
+  nameChanging = (event) => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+  validChanging = (event) => {
+    this.setState({
+      ValidThru: event.target.value.slice(0, 2) + '/' + event.target.value.slice(2, 4)
+    });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+render(){
+  return(
+    <div className='wrapping-div'>
+      <div className='credit-card' >
+        <h1 className='card'>CREDIT-CARD</h1>
+        <img src='https://cdn.freebiesupply.com/logos/large/2x/chip-1-logo-png-transparent.png' width='100px'/>
+        <div className='infos'>
+          <div className='container'>
+            <div className='Credit-number'>
+              <h5> {this.state.number.padEnd(16,'*').replace(/(.{4})/g, '$1 ')} </h5>
+            </div>
+            <div className='number-date'>
+              
+
+              <div className='code-ps'>
+                <h1 className='ValidThru'>{this.state.ValidThru.padEnd(4,'*')}</h1>
+              </div>
+            </div>
+            <p className='name'><h1>{this.state.name.toUpperCase()}</h1></p>
+          </div>
+          <img className='visa-card-logo' src='http://mcgrathpestcontrol.com/wp-content/uploads/2015/08/mcvisa.png' width='120px'/>
+        </div>
+      </div>
+      <div className='labels'>
+        <input type="text" maxLength="16" onChange={this.numberChanging} placeholder='Number' />
+        <input type="text" maxLength='20' onChange={this.nameChanging} placeholder='name' />
+        <input type="text" maxLength="4" onChange={this.validChanging} placeholder='ValidThru' />
+      </div>
+      </div>)
+}
 }
 
-export default App;
+
+
+
+export default Credit;
